e38f0aebd7f1ae865d0dbb708069c3d60d1a5db2
Mark Hall <mark@pendulumcreative.uk>
2022-12-29 01:03:24 +0000

    Refactor: Hardhat and deployment configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,11 +10,9 @@ import { NetworkUserConfig } from "hardhat/types";
 import { resolve } from "path";
 import "solidity-coverage";
 import yargs from "yargs";
-
 import "./tasks/accounts";
 
-// import "./tasks/deploy";
-
+// Set script args
 const parser = yargs
   .option("network", {
     type: "string",
@@ -22,103 +20,76 @@ const parser = yargs
   })
   .help(false)
   .version(false);
-
+  
+// Load and validate .env configs  
 dotenvConfig({ path: resolve(__dirname, "./.env") });
-
-// Ensure that we have all the environment variables we need.
 const { INFURA_API_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
-
-const DEFAULT_MNEMONIC =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
+const DEFAULT_MNEMONIC = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 (async() => {
   const argv = await parser.argv;
-  if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_API_KEY === undefined) {
+  if (["goerli", "mainnet"].includes(argv.network) && INFURA_API_KEY === undefined) {
     throw new Error(
       `Could not find Infura key in env, unable to connect to network ${argv.network}`,
     );
   }
 })();
 
-const chainIds = {
-  "harmony-mainnet": 1666600000,
-  "harmony-devnet": 1666900000,
-  hardhat: 31337,
-  mainnet: 1,
-  rinkeby: 4,
-  ganache: 1337,
-  // "arbitrum-mainnet": 42161,
-  // avalanche: 43114,
-  // bsc: 56,
-  // "optimism-mainnet": 10,
-  // "polygon-mainnet": 137,
-  // "polygon-mumbai": 80001,
-};
+// Set chain configs
+const chains: Array<NetworkUserConfig> = [
+  {
+    chainId: 1666600000, // harmony
+    url: 'https://api.s0.t.hmny.io',
+    accounts: PK ? [PK] : {
+      mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    }
+  },
+  { 
+    chainId: 1666900000, // harmonyDevnet
+    url: 'https://api.s0.ps.hmny.io',
+    accounts: PK ? [PK] : {
+      mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    }
+  },
+  { 
+    chainId: 344106930, // skaleDevnet
+    url: 'https://staging-v3.skalenodes.com/v1/staging-utter-unripe-menkar',
+    accounts: PK ? [PK] : {
+      mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    }
+  },
+  { 
+    chainId: 5, // goerli
+    url: "https://goerli.infura.io/v3/" + INFURA_API_KEY,
+    accounts: PK ? [PK] : {
+      mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    }
+  },
+  { 
+    chainId: 1337, // ganache
+    url: 'http://localhost:7545',
+    accounts: PK ? [PK] : {
+      mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+    }
+  }
+];
 
-function getChainConfig(
-  chain: keyof typeof chainIds,
-): NetworkUserConfig {
-  let jsonRpcUrl: string;
-  switch (chain) {
-    case "harmony-mainnet":
-      jsonRpcUrl = "https://api.s0.t.hmny.io";
-      break;
-    case "harmony-devnet":
-      jsonRpcUrl = "https://api.s0.ps.hmny.io/";
-      break;
-    case "ganache":
-      jsonRpcUrl = "http://localhost:7545/";
-      break;
-    default:
-      jsonRpcUrl =
-        "https://" + chain + ".infura.io/v3/" + INFURA_API_KEY;
-    // case "avalanche":
-    //   jsonRpcUrl = "https://api.avax.network/ext/bc/C/rpc";
-    //   break;
-    // case "bsc":
-    //   jsonRpcUrl = "https://bsc-dataseed1.binance.org";
-    //   break;
-  }
-  if (PK) {
-    return {
-      accounts: [PK],
-      chainId: chainIds[chain],
-      url: jsonRpcUrl,
-    };
-  } else {
-    return {
-      accounts: {
-        mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
-      },
-      chainId: chainIds[chain],
-      url: jsonRpcUrl,
-    };
-  }
+function getChainConfig(id: number) {
+  return chains.find(a => a.chainId === id)
 }
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   etherscan: {
     apiKey: {
-      harmonyDevnet: "your API key",
-      harmony: "your API key",
-      // harmonyDev: process.env.DEVNET_API_KEY || "",
       mainnet: ETHERSCAN_API_KEY || "",
-      rinkeby: ETHERSCAN_API_KEY || "",
-      // arbitrumOne: process.env.ARBISCAN_API_KEY || "",
-      // avalanche: process.env.SNOWTRACE_API_KEY || "",
-      // bsc: process.env.BSCSCAN_API_KEY || "",
-      // optimisticEthereum: process.env.OPTIMISM_API_KEY || "",
-      // polygon: process.env.POLYGONSCAN_API_KEY || "",
-      // polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+      goerli: ETHERSCAN_API_KEY || ""
     },
     customChains: [
       {
         network: "harmonyDevnet",
         chainId: 1666900000,
         urls: {
-          apiURL:
-            "https://ctrver.b.hmny.io/verify?network=devnet",
+          apiURL: "https://ctrver.b.hmny.io/verify?network=devnet",
           browserURL: "https://api.s0.ps.hmny.io",
         },
       },
@@ -133,23 +104,16 @@ const config: HardhatUserConfig = {
     showMethodSig: true,
     token: "ONE",
     currency: "USD",
-    // gasPriceApi: process.env.GASPRICE_API_ENDPOINT,
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     excludeContracts: [],
     src: "./contracts",
   },
   networks: {
-    harmony: getChainConfig("harmony-mainnet"),
-    harmonyDev: getChainConfig("harmony-devnet"),
-    mainnet: getChainConfig("mainnet"),
-    rinkeby: getChainConfig("rinkeby"),
-    ganache: getChainConfig("ganache"),
-    // arbitrum: getChainConfig("arbitrum-mainnet"),
-    // avalanche: getChainConfig("avalanche"),
-    // bsc: getChainConfig("bsc"),
-    // optimism: getChainConfig("optimism-mainnet"),
-    // "polygon-mainnet": getChainConfig("polygon-mainnet"),
-    // "polygon-mumbai": getChainConfig("polygon-mumbai"),
+    harmony: getChainConfig(1666600000),
+    harmonyDevnet: getChainConfig(1666900000),
+    skaleDevnet: getChainConfig(344106930),
+    goerli: getChainConfig(5),
+    ganache: getChainConfig(1337)
   },
   paths: {
     artifacts: "./artifacts",
@@ -176,8 +140,7 @@ const config: HardhatUserConfig = {
   dodoc: {
     runOnCompile: !!(
       process.env.GEN_DOCS && process.env.GEN_DOCS != "false"
-    ),
-    // debugMode: false,
+    )
   },
 };
 

93a915d448238e0ea8ad4d7e5055286b3596d07f
MaffazMad <john@madnfts.io>
2023-08-20 01:21:52 +0200

    [UPD] merge Router contracts

--- contracts/Router/MADRouter.sol
+++ contracts/Router/MADRouter.sol
@@ -10,7 +10,9 @@ import {
 
 import { ERC1155Basic } from "contracts/MADTokens/ERC1155/ERC1155Basic.sol";
 
-contract MADRouter1155 is MADRouterBase {
+import { ERC721Basic } from "contracts/MADTokens/ERC721/ERC721Basic.sol";
+
+contract MADRouter is MADRouterBase {
     ////////////////////////////////////////////////////////////////
     //                         CONSTRUCTOR                        //
     ////////////////////////////////////////////////////////////////
@@ -28,61 +30,39 @@ contract MADRouter1155 is MADRouterBase {
     ) MADRouterBase(_factory, _paymentTokenAddress, _recipient) { }
 
     ////////////////////////////////////////////////////////////////
-    //                       CREATOR SETTINGS                     //
+    //                    CREATOR MINTING ERC721                  //
     ////////////////////////////////////////////////////////////////
 
-    /// @notice Collection baseURI setter.
-    /// @dev Only available for Basic, Whitelist and Lazy token types. Events
-    /// logged
-    ///      by each tokens' BaseURISet functions.
-    ///      Function Sighash := 0x4328bd00
-    /// @param _token 1155 token address.
-    /// @param _uri New URI string.
-    function setBaseURI(address _token, string memory _uri) external {
-        uint8 _tokenType = _tokenRender(_token);
-
-        // if (_tokenType == 1) {
-        _checkTokenType(_tokenType);
-        ERC1155Basic(_token).setBaseURI(_uri);
-
-        emit BaseURISet(_token, _uri);
-    }
-
-    /// @notice Collection baseURI locker preventing URI updates when set.
-    ///      Cannot be unset!
-    /// @dev Only available for Basic, Whitelist and Lazy token types. Events
-    /// logged
-    ///      by each tokens' setBaseURILock functions.
-    ///      Function Sighash := ?
+    /// @notice ERC721Basic creator mint function handler.
+    /// @dev Function Sighash := 0x490f7027
     /// @param _token 721 token address.
-    function setURILock(address _token) public {
+    /// @param _to Receiver token address.
+    /// @param _amount Num tokens to mint and send.
+    function basicMintTo(address _token, address _to, uint128 _amount)
+        external
+        payable
+    {
         uint8 _tokenType = _tokenRender(_token);
-
         _checkTokenType(_tokenType);
-        ERC1155Basic(_token).setBaseURILock();
+        // _paymentCheck(0x40d097c3);
+        ERC721Basic(_token).mintTo{ value: msg.value }(_to, _amount);
     }
 
-    /// @notice Global MintState setter/controller
-    /// @dev Switch cases/control flow handling conditioned by both `_stateType`
-    /// and `_tokenType`.
-    ///      Events logged by each tokens' `setState` functions.
-    ///      Function Sighash := 0xab9acd57
-    /// @param _token 1155 token address.
-    /// @param _state Set state to true or false.
-    /// @dev _stateType Values:
-    ///      0 := PublicMintState (minimal, basic, whitelist);
-    function setMintState(address _token, bool _state) external {
-        // require(_stateType < 3, "INVALID_TYPE");
+    /// @notice Global token burn controller/single pusher for all token types.
+    /// @dev Function Sighash := 0xba36b92d
+    /// @param _token 721 token address.
+    /// @param _ids The token IDs of each token to be burnt;
+    ///        should be left empty for the `ERC721Minimal` type.
+    function burn(address _token, uint128[] memory _ids) external payable {
         uint8 _tokenType = _tokenRender(_token);
+        // _paymentCheck(0x44df8e70);
 
         _checkTokenType(_tokenType);
-        ERC1155Basic(_token).setPublicMintState(_state);
-
-        emit PublicMintState(_token, _tokenType, _state);
+        ERC721Basic(_token).burn{ value: msg.value }(_ids);
     }
 
     ////////////////////////////////////////////////////////////////
-    //                       CREATOR MINTING                      //
+    //                   CREATOR MINTING  ERC1155                 //
     ////////////////////////////////////////////////////////////////
 
     /// @notice ERC1155Basic creator mint function handler.
@@ -100,7 +80,7 @@ contract MADRouter1155 is MADRouterBase {
         uint8 _tokenType = _tokenRender(_token);
         _checkTokenType(_tokenType);
         // _paymentCheck(0x40d097c3);
-        ERC1155Basic(_token).mintTo{value: msg.value}(_to, _amount, _balance);
+        ERC1155Basic(_token).mintTo{ value: msg.value }(_to, _amount, _balance);
     }
 
     /// @dev Function Sighash := 0x535f64e7
@@ -118,7 +98,9 @@ contract MADRouter1155 is MADRouterBase {
         uint8 _tokenType = _tokenRender(_token);
         _checkTokenType(_tokenType);
         // _paymentCheck(0x40d097c3);
-        ERC1155Basic(_token).mintBatchTo{value: msg.value}(_to, _ids, _balances);
+        ERC1155Basic(_token).mintBatchTo{ value: msg.value }(
+            _to, _ids, _balances
+        );
     }
 
     /// @notice Global token burn controller/single pusher for all token types.
@@ -137,7 +119,7 @@ contract MADRouter1155 is MADRouterBase {
         uint8 _tokenType = _tokenRender(_token);
         // _paymentCheck(0x44df8e70);
         _checkTokenType(_tokenType);
-        ERC1155Basic(_token).burn{value: msg.value}(to, _ids, _amount);
+        ERC1155Basic(_token).burn{ value: msg.value }(to, _ids, _amount);
     }
 
     /// @notice Global token batch burn controller/single pusher for all token
@@ -157,33 +139,8 @@ contract MADRouter1155 is MADRouterBase {
         uint8 _tokenType = _tokenRender(_token);
 
         _checkTokenType(_tokenType);
-        ERC1155Basic(_token).burnBatch{value: msg.value}(_from, _ids, _balances);
+        ERC1155Basic(_token).burnBatch{ value: msg.value }(
+            _from, _ids, _balances
+        );
     }
-
-    ////////////////////////////////////////////////////////////////
-    //                       CREATOR WITHDRAW                     //
-    ////////////////////////////////////////////////////////////////
-
-    // /// @notice Withdraw both IERC20 and ONE from ERC1155 contract's balance.
-    // /// @dev Leave `_token` param empty for withdrawing eth only. No withdraw
-    // /// min needs to be passed as params, since
-    // ///      all balance from the token's contract is emptied.
-    // ///      Function Sighash := 0xf940e385
-    // /// @param _token 1155 token address.
-    // /// @param _erc20 IERC20 token address.
-    // /// audit B.2 BlockHat Audit  -remove whenPaused
-    // function withdraw(address _token, IERC20 _erc20) external {
-    //     uint8 _tokenType = _tokenRender(_token);
-
-    //     _checkTokenType(_tokenType);
-    //     if (address(_erc20) != address(0) && _erc20.balanceOf(_token) != 0) {
-    //         ERC1155Basic(_token).withdrawIERC20(address(_erc20), recipient);
-    //     } else if (_token.balance != 0) {
-    //         ERC1155Basic(_token).withdraw(recipient);
-    //     } else {
-    //         revert NoFunds();
-    //     }
-
-    //     emit TokenFundsWithdrawn(_token, _tokenType, msg.sender);
-    // }
 }

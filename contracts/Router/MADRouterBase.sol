4d976e5528ca9b8fdb22c6d3973f2abc7d56528d
MaffazMad <john@madnfts.io>
2023-08-20 02:41:55 +0200

    [ADD] Fee Handler

--- contracts/Router/MADRouterBase.sol
+++ contracts/Router/MADRouterBase.sol
@@ -4,12 +4,13 @@ pragma solidity 0.8.19;
 
 // solhint-disable-next-line
 import { MADBase, IERC20 } from "contracts/Shared/MADBase.sol";
+import { FeeHandler } from "contracts/Router/FeeHandler.sol";
+
 import {
     RouterEvents, FactoryVerifier
 } from "contracts/Shared/EventsAndErrors.sol";
-import { FeeOracle } from "contracts/lib/tokens/common/FeeOracle.sol";
 
-abstract contract MADRouterBase is MADBase, RouterEvents, FeeOracle {
+abstract contract MADRouterBase is MADBase, FeeHandler, RouterEvents {
     /// @notice Contract name.
     /// @dev Function Sighash := 0x06fdde03
     function name() public pure returns (string memory) {
@@ -24,27 +25,8 @@ abstract contract MADRouterBase is MADBase, RouterEvents, FeeOracle {
     //                           STORAGE                          //
     ////////////////////////////////////////////////////////////////
 
-    /// @notice Passed to feeLookup to return feeMint.
-    bytes4 internal constant _MINSAFEMINT = 0x40d097c3;
-
-    /// @notice Passed to feeLookup to return feeBurn.
-    bytes4 internal constant _MINBURN = 0x44df8e70;
-
-    /// @notice Mint fee store.
-    // audit B.3 BlockHat Audit
-    uint256 public feeMint = 0.25 ether; /* = 0 */
-
-    /// @notice Burn fee store.
-    uint256 public feeBurn; /* = 0 */
-
     /// @dev The recipient address used for public mint fees.
     address public recipient;
-
-    /// @notice max fee that can be set for mint - B.1 remove from constructor
-    uint256 public constant maxFeeMint = 2.5 ether; // 0.0003 ether
-
-    /// @notice max fee that can be set for burn - B.1 remove from constructor
-    uint256 public constant maxFeeBurn = 0.5 ether;
 
     /// @notice FactoryVerifier connecting the router to madFactory.
     FactoryVerifier public madFactory;
@@ -56,7 +38,7 @@ abstract contract MADRouterBase is MADBase, RouterEvents, FeeOracle {
     /// @notice Constructor requires a valid factory address and an optional
     /// erc20 payment token
     /// address.
-    /// @param _paymentTokenAddress erc20 token address | address(0).
+    /// @param _paymentTokenAddress erc20 token address.
     /// @param _recipient Public mint fee recipient address.
     // B.1 - Remove maxFeeMint &  maxFeeBurn from constructor
     constructor(
@@ -67,28 +49,6 @@ abstract contract MADRouterBase is MADBase, RouterEvents, FeeOracle {
         _setPaymentToken(_paymentTokenAddress);
         setFactory(_factory);
         setRecipient(_recipient);
-    }
-
-    ////////////////////////////////////////////////////////////////
-    //                         HELPERS                            //
-    ////////////////////////////////////////////////////////////////
-
-    /// @notice Mint and burn fee lookup.
-    /// @dev Function Sighash := 0xedc9e7a4
-    /// @param sigHash _MINSAFEMINT | _MINBURN
-    function feeLookup(bytes4 sigHash)
-        external
-        view
-        override(FeeOracle)
-        returns (uint256)
-    {
-        if (sigHash == _MINSAFEMINT) {
-            return feeMint;
-        } else if (sigHash == _MINBURN) {
-            return feeBurn;
-        } else {
-            return 0;
-        }
     }
 
     ////////////////////////////////////////////////////////////////
@@ -125,23 +85,21 @@ abstract contract MADRouterBase is MADBase, RouterEvents, FeeOracle {
     }
 
     /// @notice Change the Routers mint and burn fees.
-    /// @dev Event emitted by token contract.
-    ///      Function Sighash := 0x0b78f9c0
     /// @param _feeMint New mint fee.
     /// @param _feeBurn New burn fee.
-    function setFees(uint256 _feeMint, uint256 _feeBurn) external onlyOwner {
-        // require(_feeMint <= maxFeeMint && _feeBurn <= maxFeeBurn, "Invalid
-        // fee settings, beyond
-        // max");
-        assembly {
-            if or(gt(_feeMint, maxFeeMint), gt(_feeBurn, maxFeeBurn)) {
-                mstore(0x00, 0x2d8768f9)
-                revert(0x1c, 0x04)
-            }
-            sstore(feeBurn.slot, _feeBurn)
-            sstore(feeMint.slot, _feeMint)
-        }
+    function setFees(uint256 _feeMint, uint256 _feeBurn) public onlyOwner {
+        _setFees(_feeMint, _feeBurn);
+        emit FeesUpdated(_feeMint, _feeBurn);
+    }
 
+    /// @notice Change the Routers mint and burn fees for erc20 Tokens.
+    /// @param _feeMint New mint fee.
+    /// @param _feeBurn New burn fee.
+    function setFees(uint256 _feeMint, uint256 _feeBurn, address erc20Address)
+        public
+        onlyOwner
+    {
+        _setFees(_feeMint, _feeBurn, erc20Address);
         emit FeesUpdated(_feeMint, _feeBurn);
     }
 
--- contracts/lib/tokens/common/FeeOracle.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-pragma solidity 0.8.19;
-
-interface FeeOracle {
-    function feeLookup(bytes4 sigHash) external view returns (uint256 fee);
-}

e2a15e523b958828dad2dd17ed4a68cbf74cbdcf
Maffaz <john@madnfts.io>
2023-11-22 23:19:33 +0100

    [UPD] invariants tests

--- test/foundry/Base/Deploy/deployerBase.sol
+++ test/foundry/Base/Deploy/deployerBase.sol
@@ -49,9 +49,9 @@ abstract contract DeployerBase is
     address swapRouter = makeAddr("SwapRouter");
 
     uint256 public erc20MintFee = 1 ether;
-    uint256 public erc20BurnFee = 1 ether;
+    uint256 public erc20BurnFee = 0.5 ether;
     uint256 public erc20CreateSplitterFee = 1 ether;
-    uint256 public erc20CreateCollectionFee = 1 ether;
+    uint256 public erc20CreateCollectionFee = 0.5 ether;
 
     function deployAll(ercTypes ercType, bool isERC20)
         public

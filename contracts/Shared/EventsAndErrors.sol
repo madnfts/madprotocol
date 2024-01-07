e822c6c29bf79d1c3714bf5888a368acc2c6353c
MaffazMad <john@madnfts.io>
2024-01-07 19:39:21 +0100

    [FIX] imports

--- contracts/Shared/EventsAndErrors.sol
+++ contracts/Shared/EventsAndErrors.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.22;
 
 import { FactoryVerifier } from "contracts/lib/auth/FactoryVerifier.sol";
-import { IERC721, IERC1155 } from "contracts/Shared/MarketplaceTypes.sol";
 
 interface EventsAndErrorsBase {
     event PaymentTokenUpdated(address indexed newPaymentToken);
